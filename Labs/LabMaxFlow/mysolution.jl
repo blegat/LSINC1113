8cca6681741d8f9ade04aa900987f81361b3db05
mdausort <manon.dausort@uclouvain.be>
2025-11-10 11:16:19 +0100

    LabMaxFlow

---
+++ Labs/LabMaxFlow/mysolution.jl
@@ -0,0 +1,18 @@
+# TODO: Implémenter l'algorithme de Max-Flow pour trouver le nombre maximum de scans simultanés
+function find_maximum_scans(scanners::Vector{Scanner}, employees::Vector{Employee})
+    # Votre implémentation ici
+    # Utilisez l'algorithme de Ford-Fulkerson ou Edmonds-Karp
+    # Retournez (nombre_maximum_scans, assignations_scanner_employé)
+    
+    error("À implémenter : utilisez l'algorithme de Max-Flow pour trouver le nombre maximum de scans simultanés")
+end
+
+# TODO: Implémenter la fonction pour trouver les formations optimales
+function find_optimal_training(scanners::Vector{Scanner}, employees::Vector{Employee})
+    # Votre implémentation ici
+    # Trouvez quels employés former sur quels scanners pour maximiser la capacité
+    # Retournez (formations_recommandées, nouvelle_capacité_maximale)
+    
+    error("À implémenter : trouvez les formations optimales pour maximiser la capacité de scan")
+end
+
---
+++ Labs/LabMaxFlow/solution/README.md
@@ -0,0 +1,3 @@
+> [!WARNING]
+> Ne regardez la solution avant d'avoir fait l'exercice,
+> sinon l'exercice ne vous préparera pas aussi bien à l'examen
